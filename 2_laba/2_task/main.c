fc79744acf88c9c35336cf3d2424ac1668710525
ledza228 <leshyash2003@gmail.com>
2022-03-10 18:28:45 +0300

    2_task for 2_lab

---
+++ 2_laba/2_task/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main (int argc, char *argv[]){
+	
+
+	if (argc < 3){
+		printf("usage: %s <file_name> <amount_of_output_strings>", argv[0]);
+		return EXIT_FAILURE;
+	}
+
+	char * file_name = argv[1];
+	
+	char* setting_string = argv[2];
+	int setting = atoi(setting_string);
+
+	
+	FILE* file = fopen(file_name, "r");
+	
+	if (file == NULL){
+		printf("Error: can't open file %s", argv[1]);
+		return EXIT_FAILURE;
+	}
+
+
+	char symb;
+	int amnt_of_lines = 0;
+	while ((symb = fgetc(file)) != EOF){
+		if (symb == '\n')
+			amnt_of_lines++;
+		if (amnt_of_lines == setting && setting != 0){
+			getc(stdin);
+			amnt_of_lines = 0;
+		}
+
+		putc(symb, stdout);
+
+	}
+
+
+}
